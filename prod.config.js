aa9ddc478ad28899c5c316ff4c4a0639af662f14
Jithin Sebastian <jithin.s@mozilor.com>
2021-11-22 02:08:47 +0530

    Added post build config edit

--- prod.config.js
+++ prod.config.js
@@ -3,10 +3,6 @@ module.exports = {
     {
       name: "Employee Manager APP",
       script: "./index.js",
-      node_args: "-r dotenv/config",
-      env: {
-        DOTENV_CONFIG_PATH: ".env",
-      },
     },
   ],
 };
